3d1497262453d41e815b2648886ea50b671c9357
Tunji Akinbami <takinbami@springboardservices.ca>
2016-11-25 16:44:53 -0500

    new changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
-  return (
-    <h2 className="text-center">
-      {props.headerMessage}
-    </h2>
-  );
-};
-
-App.propTypes = {
-  headerMessage: React.PropTypes.string.isRequired
-};
-
-App.defaultProps = {
-  headerMessage: 'Hello!'
-};
+import App from './components/App';
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+
+App.defaultProps = {
+  headerMessage: 'Hellos!'
+};
